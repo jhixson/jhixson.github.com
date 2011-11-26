9aac81cddbe010cdaf08ef9912c8a47926d922e1
Jesse Hixson <hixsonj@gmail.com>
2011-11-26 12:08:30 -0500

    Updates to nav/layout.

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,13 @@
 var _t;
 $(document).ready(function() {
-	$('.links li').mouseenter(function(){
+	$('.links').delegate('li','mouseenter', function(){
 		$('a',this).animate({'color':'#fff','backgroundColor':'#7F4B16','backgroundPosition':'268px 3px'},250,'easeOutBack');
 		$('span',this).show(100);
 		clearTimeout(_t);
-	}).mouseleave(function(){
+	}).delegate('li','mouseleave',function(){
 		$('a',this).animate({'color':'#eee','backgroundColor':'#B26110','backgroundPosition':'317px 3px'},250,'easeInBack');
 		$('span',this).hide(100);
-		if(current < $('.overflow .links li').length)
+		if(_current < $('.overflow .links li').length)
 			_t = setTimeout(scrollLinks,5000);
 	});
 	
@@ -38,6 +38,11 @@ $(document).ready(function() {
 			_t = setTimeout(scrollLinks,5000);
 		});
 	});
+	
+	getReddit();
+	if('.photobox')
+		getPhotos();
+		
 });
 
 $(window).load(function() {
@@ -65,4 +70,63 @@ function scrollLinks() {
 	
 	if(_current < $('.overflow .links li').length)
 		_t = setTimeout(scrollLinks,5000);
+}
+
+function getPhotos() {
+	$.getJSON('https://api.instagram.com/v1/users/319716/media/recent?access_token=319716.7a1643b.71fae0877eaf4eb090a3d4a316268126&callback=?',function(json) {
+		console.log(json);
+		$.each(json.data,function() {
+				$('#slider ul').append('<li><img src="'+this.images.low_resolution.url+'" alt="" /></li>');
+				$('#strip ul').append('<li><img src="'+this.images.low_resolution.url+'" alt="" width="54" height="54" /></li>');
+		});
+		
+		var img_count = $('#slider li').length;
+		$('#slider ul').width(306*img_count);
+		//$('#strip ul').width(54*img_count);
+		
+		$('#next_photo').click(function(e) {
+				e.preventDefault();
+				var ul = $('#slider ul:not(:animated)');
+				var max = ul.width() - 306;
+				var right = parseInt(ul.css('right'));
+				ul.animate({'right': Math.min(right+306,max)},550,'easeOutQuart');
+		});
+		
+		$('#prev_photo').click(function(e) {
+				e.preventDefault();
+				var ul = $('#slider ul:not(:animated)');
+				var right = parseInt(ul.css('right'));
+				ul.animate({'right': Math.max(right-306,0)},550,'easeOutQuart');
+		});
+		
+		$('#strip li').click(function(e) {
+				//$(this).addClass('active');
+				var pos = $('#strip li').index($(this));
+				var ul = $('#slider ul:not(:animated)');
+				var max = ul.width() - 306;
+				ul.animate({'right': pos*306},550,'easeOutQuart');
+		});
+		
+		/*
+		$('#slider').easySlider({
+			auto: false,
+			continuous: true
+		});
+		
+		$('#slider').bxGallery({
+			maxheight: 306,
+			thumbwidth: 75,
+			thumbplacement: 'bottom',
+			thumbcontainer: 90
+		});
+		*/
+	});
+}
+
+function getReddit() {
+	$.getJSON('http://www.reddit.com/user/hixsonj/liked/.json?jsonp=?',function(json) {
+		$.each(json.data.children, function() {
+			$('ul.links').append('<li><a href="'+this.data.url+'">'+this.data.title+'</a><span>'+this.data.subreddit+'</span></li>');
+		});
+	});
 }
